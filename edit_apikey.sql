7b5b8176fcc5956c8f3e2e2ba841fd068595869c
Hannah Ward <Hannah.ward9001@gmail.com>
2019-02-11 17:16:44 +0000

    new: Automatically generate org

--- edit_apikey.sql
+++ edit_apikey.sql
@@ -1,1 +1,3 @@
 INSERT INTO users(org_id, server_id, role_id, authkey, email, termsaccepted, disabled, password, last_login, current_login) VALUES (1, 0, 1, 'testmispapikeytestmispapikeytestmispapik', 'test@test.com', 1, 0, 'totally a password', 0, 1549897515);
+insert into organisations (name, date_created, date_modified, description, type, created_by, uuid, local) values ("ORGNAME", "2019-02-11 17:12:46", "2019-02-11 17:12:46", "Test org", "ADMIN", 0, "5c61ad0e-30f8-4514-8af5-0144ac110003", 1);
+
--- run.sh
+++ run.sh
@@ -12,6 +12,11 @@ set -e
 service mysql start
 mysql -u root --password=misp misp < /edit_apikey.sql
 
+intial_login() { sleep 5; curl --header "Content-type: application/json" --header "Accept: application/json" --header "Authorization: testmispapikeytestmispapikeytestmispapik" -D- http://127.0.0.1/users/login --data-binary '{"User": {"username": "admin@admin.test", "password": "test"}}'
+}
+
+intial_login &
+
 cd /
 exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
           
